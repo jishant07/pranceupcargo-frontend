0033f8bb08f972c95a5d1c75a6b44b31898d4029
jishant07 <jishanta@gmail.com>
2022-03-30 12:43:05 -0400

    Added Credentials to Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyA3NztBcAEmwlD411cChV-naztaH3ky5Qk",
+    authDomain: "pranceup-cargo.firebaseapp.com",
+    projectId: "pranceup-cargo",
+    storageBucket: "pranceup-cargo.appspot.com",
+    messagingSenderId: "660075239942",
+    appId: "1:660075239942:web:b439b57652979973604187",
+    measurementId: "G-DDG8ML3S12"
+  },
+  devURL:"http://localhost:5001/pranceup-cargo/us-central1/api"
 };
