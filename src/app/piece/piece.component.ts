84b61803bff0737a6a4df4059a8961dc9cef1023
Deepikarssb <deepikakapoor.sd@gmail.com>
2022-05-25 11:59:07 +0530

    Removed unwanted code

--- src/app/piece/piece.component.ts
+++ src/app/piece/piece.component.ts
@@ -19,13 +19,6 @@ export class PieceComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  // ngOnDestroy() {
-  //   var t;
-  //   for(t = this.PicesArray.length; t > 0; t--) {
-  //     this.PicesArray.pop();
-  //   }
-  // }
-
   static makePieceItem(){
 
     if(localStorage.getItem('inco_terms') == 'ddp' || localStorage.getItem('inco_terms') == 'ddu') {
@@ -57,10 +50,6 @@ export class PieceComponent implements OnInit {
     }
   }
   
-  // deleteRow(rowNumber:number){
-  //   console.log('Row number is '+rowNumber);
-  // }
-
   openhidehs_codediv() {
     if(localStorage.getItem('inco_terms') == 'ddp' || localStorage.getItem('inco_terms') == 'ddu') {
       this.hs_codediv = false;
@@ -68,7 +57,6 @@ export class PieceComponent implements OnInit {
       this.hs_codediv = true;
     }
   }
-
 
   getClass() {
     console.log(localStorage.getItem('inco_terms'));
@@ -79,141 +67,10 @@ export class PieceComponent implements OnInit {
     }
   }
 
-
   deleteRow(event:any){
     const parentElement = this.elRef.nativeElement.closest('.pieces-class');
     this.quotation.removePackage(parentElement);
-  }
-//-------------------------------------------------------------
-// //Working approach
-// @Input() pieceForm :FormGroup;
-
-// PicesArray: Array<PicesModel> = [];  
-// newDynamic: any = {};    
-
-// ngOnInit(): void {  
-//   this.pieceForm = new FormGroup({
-//     cargoType: new FormControl('',[Validators.required]),
-//     noOfPieces: new FormControl(null,[Validators.required]),
-//     length: new FormControl(null,[Validators.required]),
-//     breath: new FormControl(null,[Validators.required]),
-//     height: new FormControl(null,[Validators.required]),
-//     inchOrCm: new FormControl(),
-//     grossWeight: new FormControl(null,[Validators.required])  
-//   });
-
-//   this.addRow();
-// }  
-
-// addRow() {    
-//   //this.newDynamic = {cargoType: ' ', noOfPieces: '',length: ' ',breath: ' ',height: ' ',inchOrCm: ' ',grossWeight: ' '};  
-//   this.newDynamic = new FormGroup({
-//     cargoType: new FormControl('',[Validators.required]),
-//     noOfPieces: new FormControl(null,[Validators.required]),
-//     length: new FormControl(null,[Validators.required]),
-//     breath: new FormControl(null,[Validators.required]),
-//     height: new FormControl(null,[Validators.required]),
-//     inchOrCm: new FormControl(),
-//     grossWeight: new FormControl(null,[Validators.required])  
-//   });
-//   // this.newDynamic.get('cargoType')?.setValue('');
-//   // this.newDynamic.get('noOfPieces')?.setValue('');
-//   // this.newDynamic.get('length')?.setValue('');
-//   // this.newDynamic.get('breath')?.setValue('');
-//   // this.newDynamic.get('height')?.setValue('');
-//   // this.newDynamic.get('inchOrCm')?.setValue('');
-//   // this.newDynamic.get('grossWeight')?.setValue('');
-
-//   this.PicesArray.push(this.newDynamic);
-  
-//   // ((this.pieceForm.get('controls') as FormArray).at(index) as FormGroup).get('cargoType')?.setValue('');
-//   // ((this.pieceForm.get('controls') as FormArray).at(index) as FormGroup).get('noOfPieces')?.setValue('');
-//   // ((this.pieceForm.get('controls') as FormArray).at(index) as FormGroup).get('length')?.setValue('');
-//   // ((this.pieceForm.get('controls') as FormArray).at(index) as FormGroup).get('breath')?.setValue('');
-//   // ((this.pieceForm.get('controls') as FormArray).at(index) as FormGroup).get('height')?.setValue('');
-//   // ((this.pieceForm.get('controls') as FormArray).at(index) as FormGroup).get('inchOrCm')?.setValue('');
-//   // ((this.pieceForm.get('controls') as FormArray).at(index) as FormGroup).get('grossWeight')?.setValue('');
-  
-
-//   //this.resetTags(index);
-
-//   return true;  
-// } 
-
-// // resetTags(index:number) {
-// //   index = index + 1;
-// //   debugger
-// //   const arr = this.pieceForm.get('controls') as FormArray;
-// //   while (index !== arr.length) {
-// //     arr.removeAt(index);
-// //   }
-// // }
-  
-// deleteRow(index:number) {  
-//   if(this.PicesArray.length ==1) { 
-//      //when only one row  
-//        return false;  
-//   } else {  
-//     //delete row
-//     this.PicesArray.splice(index, 1);          
-//     return true;  
-//   }  
-// }
-
-//------------------------------------------------
-  // parentFormGroup: FormGroup;
-  // pieceFormFormGroup: FormGroup;
-
-  // constructor(private fb: FormBuilder, private parentControl: ControlContainer) {
-  // }
-
-  // ngOnInit(): void {
-  //   this.parentFormGroup = this.parentControl.control as FormGroup;
-  //   this.pieceFormFormGroup = this.fb.group({
-  //     cargoType: new FormControl('',[Validators.required]),
-  //     noOfPieces: new FormControl(null,[Validators.required]),
-  //     length: new FormControl(null,[Validators.required]),
-  //     breath: new FormControl(null,[Validators.required]),
-  //     height: new FormControl(null,[Validators.required]),
-  //     inchOrCm: new FormControl(),
-  //     grossWeight: new FormControl(null,[Validators.required]) 
-  //   });
-  //   this.parentFormGroup.addControl('pieces', this.pieceFormFormGroup);
-  // }
-
-
-  // public get isValid(): boolean {
-  //   return this.addressFormGroup.valid;
-  // }
-
-  // public get isDirty(): boolean {
-  //   return this.addressFormGroup.dirty;
-  // }
-//------------------------------------------------
-
-// @Input() parentFormGroup: FormGroup;
-
-// constructor(private fb: FormBuilder) { }
-
-//   ngOnInit(): void {
-//     this.parentFormGroup.addControl('pieces', this.fb.group({
-//       cargoType: new FormControl('',[Validators.required]),
-//       noOfPieces: new FormControl(null,[Validators.required]),
-//       length: new FormControl(null,[Validators.required]),
-//       breath: new FormControl(null,[Validators.required]),
-//       height: new FormControl(null,[Validators.required]),
-//       inchOrCm: new FormControl(),
-//       grossWeight: new FormControl(null,[Validators.required]) 
-//     }));
-//   }
-// public get isValid(): boolean {
-//   return this.parentFormGroup.controls.pieces.valid;
-// }
-
-// public get isDirty(): boolean {
-//   return this.parentFormGroup.controls.pieces.dirty;
-// }
-  
+  }  
 }
 
 export class PicesModel{     
