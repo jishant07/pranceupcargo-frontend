eddcb2b897b23a8ed6be9e750e4b67c2132ec125
Deepikarssb <deepikakapoor.sd@gmail.com>
2022-07-01 23:06:40 +0530

    Place Order Page- API integration issue

---
+++ src/app/_services/order.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { GlobalService } from './global.service';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  token?:string; 
+  httpHeader:any;
+  isLoading:boolean = false;
+
+  constructor(
+      private http:HttpClient
+      ,private globalservice:GlobalService) { }
+
+  getToken(){
+    this.token = localStorage.getItem('token')!;
+  }
+  setHeader(){
+    this.getToken();
+    this.httpHeader = {
+        'token': this.token as string,
+        
+    };
+  }
+
+  //Begin: Order
+  placeOrder(data:FormData){
+      console.log('Service placeOrder');
+      console.log(data);      
+      return this.http.post(environment.devURL+"/order/placeNewOrder", data, {headers: this.httpHeader});
+      //{status: 'success', message: 'qtyOFyP20ClbVz7Syz06'}
+  }
+//   getQuotes(){
+//       return this.http.get(environment.devURL+"/quote/listAllQuotes",{headers: this.httpHeader});
+//       //{status: 'success', message: Array(9)}
+//   }
+  //End: Order
+}
